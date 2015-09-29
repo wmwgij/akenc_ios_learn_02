066fec179f00bc97de8d1bd0af4b383e2782839d
wmwgij <wmwgij@hotmail.com>
2015-09-29 15:16:50 +0800

    add ThridViewController files

---
+++ akenc_ios_learn_02/akenc_ios_learn_02/ThirdViewController.h
@@ -0,0 +1,15 @@
+//
+//  ThreeViewController.h
+//  akenc_ios_learn_02
+//
+//  Created by kenshino on 15/9/29.
+//  Copyright © 2015年 akenc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+
+@end
+
---
+++ akenc_ios_learn_02/akenc_ios_learn_02/ThirdViewController.m
@@ -0,0 +1,27 @@
+//
+//  ThreeViewController.m
+//  akenc_ios_learn_02
+//
+//  Created by kenshino on 15/9/29.
+//  Copyright © 2015年 akenc. All rights reserved.
+//
+
+#import "ThirdViewController.h"
+
+@interface ThirdViewController ()
+
+@end
+
+@implementation ThirdViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
